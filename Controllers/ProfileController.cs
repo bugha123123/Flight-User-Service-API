8c97b1c4c95415f01a25aba016f470f47da6e48e
bugha123123 <irakliberdzena314@gmail.com>
2023-12-27 01:04:53 +0400

    Profile update done

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookFlight.Model;
+using BookFlight.Data;
+
+namespace BookFlight.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // PUT: api/Profile/{email}
+        [HttpPut("{email}")]
+        public IActionResult UpdateUserProfile(string email, [FromBody] UserModel updatedUser)
+        {
+            var existingUser = _context.Users.FirstOrDefault(u => u.EmailAddress == email);
+
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            // Update properties
+            existingUser.UserName = updatedUser.UserName;
+            existingUser.EmailAddress = updatedUser.EmailAddress;
+            existingUser.Password = updatedUser.Password;
+            // You can update other properties as needed
+
+            // Save changes
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating profile.");
+            }
+
+            return Ok("Profile updated successfully");
+        }
+    }
+}
